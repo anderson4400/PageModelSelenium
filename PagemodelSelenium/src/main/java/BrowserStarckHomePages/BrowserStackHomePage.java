78d97b9a85ce9f4d220602e0a14111df61827233
anderson440040@gmail.com <anderson440040@gmail.com>
2022-11-07 15:51:14 -0400

    Optimizacion de codigo con @FindBy(PageFactory)

--- PagemodelSelenium/src/main/java/BrowserStarckHomePages/BrowserStackHomePage.java
+++ PagemodelSelenium/src/main/java/BrowserStarckHomePages/BrowserStackHomePage.java
@@ -2,21 +2,30 @@ package BrowserStarckHomePages;
 import static org.testng.Assert.assertEquals;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class BrowserStackHomePage {
 
     WebDriver driver;
-    By Header=By.xpath("//h1");
-    By getStarted=By.xpath("//*[@id='signupModalButton']");
+
+
+    //Se cambiaron los FindElements por @FindBy para utilizar el PageFactory y inicializar los elementos web
+    //De esta maneta optimizamos mas el codigo y utilizamos buenas practicas a la hora de automatizar
+    @FindBy (xpath = "//h1")  WebElement Header;
+    @FindBy (xpath = "//*[@id='signupModalButton']")  WebElement getStarted;
 
     public BrowserStackHomePage(WebDriver driver) {
         this.driver=driver;
+        PageFactory.initElements(driver,this);
     }
 
     public void veryHeader() {
-        String getheadertext=driver.findElement(Header).getText();
+        String getheadertext=Header.getText();
         assertEquals("App & Browser Testing Made Easy", getheadertext);
     }
     public void clickOnGetStarted() {
-        driver.findElement(getStarted).click();
+        getStarted.click();
     }
 }
--- PagemodelSelenium/src/main/java/BrowserStarckHomePages/BrowserStackSignUpPage.java
+++ PagemodelSelenium/src/main/java/BrowserStarckHomePages/BrowserStackSignUpPage.java
@@ -4,29 +4,38 @@ package BrowserStarckHomePages;
 import static org.testng.Assert.assertEquals;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class BrowserStackSignUpPage {
     WebDriver driver;
-    By Header = By.xpath("//h1");
-    By userName = By.xpath("//*[@id='user_full_name']");
-    By businessEmail = By.xpath("//*[@id='user_email_login']");
-    By password = By.xpath("//*[@id='user_password']");
+
+
+    //Se cambiaron los FindElements por @FindBy para utilizar el PageFactory y inicializar los elementos web
+    //De esta maneta optimizamos mas el codigo y utilizamos buenas practicas a la hora de automatizar
+    @FindBy (xpath = "//h1") WebElement Header;
+    @FindBy (xpath = "//*[@id='user_full_name']") WebElement userName;
+    @FindBy (xpath = "//*[@id='user_email_login']") WebElement businessEmail;
+    @FindBy (xpath = "//*[@id='user_password']") WebElement password;
+
 
     public BrowserStackSignUpPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver,this);
     }
 
     public void veryHeader() {
-        String getheadertext = driver.findElement(Header).getText().trim();
+        String getheadertext = Header.getText().trim();
         assertEquals("Create a FREE Account", getheadertext);
     }
     public void enterFullName(String arg1) {
-        driver.findElement(userName).sendKeys(arg1);
+       userName.sendKeys(arg1);
     }
     public void enterBusinessEmail(String arg1) {
-        driver.findElement(businessEmail).sendKeys(arg1);
+        businessEmail.sendKeys(arg1);
     }
     public void enterPasswrod(String arg1) {
-        driver.findElement(password).sendKeys(arg1);
+        password.sendKeys(arg1);
     }
 }
